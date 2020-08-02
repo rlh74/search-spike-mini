aed1945d4b0ae0e3dacfcbee3936b9d98037f5a3
Ross Hutchens <rlhcxm@gmail.com>
2020-08-02 13:32:00 -0500

    Setup search input to send query and return results from OpenLibrary Search API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    query: '',
+    results: []
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    console.log('query entry is:', event.target.value)
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  handleClick = () => {
+    console.log('query when search button clicked is:', this.state.query)
+    this.getBooks(this.state.query);
+  }
+
+  getBooks = (query) => {
+    console.log('searching with query:', query)
+    axios.get(`http://openlibrary.org/search.json?q=${query}`).then((response)=>{
+      console.log('back from GET:', response.data.docs);
+      this.setState({
+        results: response.data.docs
+      })
+    }).catch((err)=>{
+      console.log('error with GET', err);
+    })  }
+
+  render(){
+    return (
+      <>
+      <input onChange={this.handleChange} placeholder="search"></input>
+      <button onClick={this.handleClick}>Search</button>
+      {this.state.results ? this.state.results.map((item, index)=>{
+          return <div key={index}>
+            <p>{item.title_suggest}, {item.author_name}, {item.publish_year ? item.publish_year.shift() : 'N/A'}, Published by: {item.publisher ? item.publisher.shift() : 'N/A'}</p>
+            </div>
+        }) : '' }
+      </>
+      );
+  }
 }
 
 export default App;
